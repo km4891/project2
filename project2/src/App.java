00a44a9179422757e3aed449a3a377172a32104a
km4891 <kmorarity@gmail.com>
2021-06-22 18:16:39 -0700

    sumInt done

--- project2/src/App.java
+++ project2/src/App.java
@@ -1,5 +1,22 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Inout 2 numbers, with 2nd being larger");
+
+        int begin = input.nextInt();
+        int end = input.nextInt();
+
+        System.out.println(sumInts(begin, end));
+    }
+
+    public static int sumInts(int begin, int end){
+        
+        if (begin == end)
+            return end; // return the last number
+        else
+            return begin + sumInts(begin+1,end); // sum the current number with the recursion result
     }
 }
